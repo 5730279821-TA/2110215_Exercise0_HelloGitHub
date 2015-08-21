09c13864621e857811303d224397ef33f7c0bfd9
5730279821-TA <mixtelecom8@gmail.com>
2015-08-21 10:53:58 +0700

    Implement Message

--- src/logic/Hello.java
+++ src/logic/Hello.java
@@ -15,7 +15,7 @@ public class Hello {
 
 	public static String greetingMessage(){
 		// TODO Implement this method to return string "Hello GitHub".
-		return "";
+		return "Mix_Tera";
 	}
 	
 	public static int[] bubbleSort(int[] array){
